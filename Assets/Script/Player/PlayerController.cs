1b5b24f07a2fe8277edf7c0b4e6d818cbd8a5f58
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-11-04 16:28:46 -0300

    Att

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -4,9 +4,6 @@ public class PlayerController : MonoBehaviour
 {
     static public PlayerController instance;
 
-    private Transform currentDoor;
-    private Vector2 doorPosition;
-
     void Awake()
     {
         if (instance == null)
@@ -17,21 +14,6 @@ public class PlayerController : MonoBehaviour
         else
         {
             Destroy(gameObject);
-        }
-    }
-
-    public void EnterDoor(Transform door)
-    {
-        currentDoor = door;
-        doorPosition = transform.position;
-    }
-
-    public void ExitDoor()
-    {
-        if (currentDoor != null)
-        {
-            transform.position = doorPosition;
-            currentDoor = null;
         }
     }
 }
