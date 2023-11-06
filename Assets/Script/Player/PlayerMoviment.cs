5cbe7d7e3b59591f81f4e74041d831bea607884b
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-11-06 17:12:17 -0300

    Att

--- Assets/Script/Player/PlayerMoviment.cs
+++ Assets/Script/Player/PlayerMoviment.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMoviment : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
     [SerializeField] private float playerSpeed = 5f;
     private Vector2 direction;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        WalkAnimation();
     }
 
     void FixedUpdate()
@@ -26,6 +24,19 @@ public class PlayerMoviment : MonoBehaviour
         else if (direction.x > 0)
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+    }
+
+    private void WalkAnimation()
+    {
+        if(direction.magnitude > 0)
+        {
+            anim.SetBool("isWalk", true);
+        }
+        else
+        {
+            anim.SetBool("isWalk", false);
         }
     }
 
