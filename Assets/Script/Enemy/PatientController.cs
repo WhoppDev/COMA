708f7431d252a860279cdaf7b8ba4676c33f286d
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-11-19 01:38:58 -0300

    Att Attack Patient

--- Assets/Script/Enemy/PatientController.cs
+++ Assets/Script/Enemy/PatientController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PatientController : MonoBehaviour
 {
     [SerializeField] private EnemyStatus enemyStatus;
+    [SerializeField] private Animator anim;
     [SerializeField] private ParticleSystem bloodParticle;
+
+    [SerializeField] private Collider2D enemyView;
+    [SerializeField] private Collider2D enemyAttack;
 
 
     [SerializeField] private float currentEnemyLife;
+    public float attackRange;
 
     private void Start()
     {
@@ -21,6 +26,13 @@ public class PatientController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        float distanceToPlayer = Vector3.Distance(CORE.instance.player.transform.position, this.transform.position);
+        if(distanceToPlayer < attackRange) 
+        {
+            anim.SetTrigger("Attack");
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,5 +43,8 @@ public class PatientController : MonoBehaviour
             bloodParticle.Play();
             Debug.Log(currentEnemyLife);
         }
+
+
+
     }
 }
